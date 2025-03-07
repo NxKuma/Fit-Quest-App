0689d661d15afe6c4f83c4cd6a8551d2c47d661a
thanielLee <thaniel.lee@student.ateneo.edu>
2025-03-07 20:37:31 +0800

    workout routine resource added

---
+++ scripts/workout_routine.gd
@@ -0,0 +1,19 @@
+extends Resource
+class_name workout_routine
+
+@export var exercises: Array[Exercise]
+@export var routine_name: String
+
+var current_exercises: Array[Exercise] = exercises
+signal workout_finished
+signal routine_finished
+
+func finish_workout() -> void:
+	exercises.pop_front()
+	emit_signal("workout_finished")
+	if exercises.size() == 0:
+		finish_routine()
+		
+
+func finish_routine() -> void:
+	emit_signal("routine_finished")
