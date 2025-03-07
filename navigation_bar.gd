dc8df954278cd3dffff635a9aeeba6fe0ab68a40
Marco <marco.gimena@student.ateneo.edu>
2025-03-07 19:51:50 +0800

    reverted navigation bar logic

--- navigation_bar.gd
+++ navigation_bar.gd
@@ -1,26 +1,17 @@
 extends MarginContainer
 
-@onready var workout_button: Button = $"Buttons/Workout Button"
-@onready var tutorial_button: Button = $"Buttons/Tutorial Button"
-@onready var home_button: Button = $"Buttons/Home Button"
-@onready var guilds_button: Button = $"Buttons/Guilds Button"
-@onready var profile_button: Button = $"Buttons/Profile Button"
+var initialView := 2
 
-@export var workout_view: PackedScene
-@export var tutorial_view: PackedScene
-@export var home_view: PackedScene
-@export var guilds_view: PackedScene
-@export var profile_view: PackedScene
+@onready var views: MarginContainer = $"../../Views"
+@onready var buttons: HBoxContainer = %Buttons
 
 func _ready():
-	connectButton(workout_button, workout_view)
-	connectButton(tutorial_button, tutorial_view)
-	connectButton(home_button, home_view)
-	connectButton(guilds_button, guilds_view)
-	connectButton(profile_button, profile_view)
+	_set_page(initialView)
+	
+	for button in buttons.get_children():
+		var buttonIndex = button.get_index()
+		button.pressed.connect(_set_page.bind(buttonIndex))
 
-func connectButton(button: Button, scene: PackedScene):
-	button.pressed.connect(changeToScene.bind(scene))
-
-func changeToScene(scene: PackedScene):
-	get_tree().change_scene_to_packed(scene)
+func _set_page(viewNumber):
+	for i in views.get_child_count():
+		views.get_children()[i].visible = i == viewNumber # set visible if i == viewNumber
