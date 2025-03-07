55bea43e82b58aad120521e02c186fc139631eaf
Marco <marco.gimena@student.ateneo.edu>
2025-03-07 19:19:47 +0800

    revamped naviagation bar logic

--- navigation_bar.gd
+++ navigation_bar.gd
@@ -1,14 +1,26 @@
 extends MarginContainer
 
-@export var home_view: PackedScene
+@onready var workout_button: Button = $"Buttons/Workout Button"
+@onready var tutorial_button: Button = $"Buttons/Tutorial Button"
+@onready var home_button: Button = $"Buttons/Home Button"
+@onready var guilds_button: Button = $"Buttons/Guilds Button"
+@onready var profile_button: Button = $"Buttons/Profile Button"
+
 @export var workout_view: PackedScene
-@export var guilds_view: PackedScene
 @export var tutorial_view: PackedScene
+@export var home_view: PackedScene
+@export var guilds_view: PackedScene
 @export var profile_view: PackedScene
 
+func _ready():
+	connectButton(workout_button, workout_view)
+	connectButton(tutorial_button, tutorial_view)
+	connectButton(home_button, home_view)
+	connectButton(guilds_button, guilds_view)
+	connectButton(profile_button, profile_view)
 
 func connectButton(button: Button, scene: PackedScene):
-	button.pressed.connect(changeToScene(scene))
+	button.pressed.connect(changeToScene.bind(scene))
 
 func changeToScene(scene: PackedScene):
 	get_tree().change_scene_to_packed(scene)
