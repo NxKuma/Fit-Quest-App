54c9da88e4a2b2bd9e54a148c43fa768d97b3d4d
Kuma <55661040+NxKuma@users.noreply.github.com>
2025-05-04 14:55:41 +0800

    Changed Labels to Appropriate name

--- scripts/home_view.gd
+++ scripts/home_view.gd
@@ -1,6 +1,12 @@
 extends Control
 
 
+@onready var day_of_the_week: Label = $"Side Panel Margin/Side Panels/VBoxContainer/MarginContainer/HSplitContainer/Day of the Week"
+@onready var streak: RichTextLabel = $"Side Panel Margin/Side Panels/VBoxContainer/MarginContainer/HSplitContainer/Streak"
+@onready var routine_of_the__day: Label = $"Side Panel Margin/Side Panels/VBoxContainer/Panel/VSplitContainer/MarginContainer2/HSplitContainer/Routine of the  Day"
+@onready var target_muscle: Label = $"Side Panel Margin/Side Panels/VBoxContainer/Panel2/VSplitContainer/MarginContainer2/HSplitContainer/Target Muscle"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
