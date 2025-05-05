850a93e26c469e065c570cb1fb0e590bd822be2f
JohnverLavina <johnver.lavina@student.ateneo.edu>
2025-05-06 05:15:22 +0800

    revised UI, added new models, reformated nav bar

--- views/profile_view.gd
+++ views/profile_view.gd
@@ -1,10 +1,11 @@
 extends Control
 
+@onready var character_3d: SubViewportContainer = $CircleMask2/Character3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var anim_player = character_3d.get_node("SubViewport/character_model_scene/AnimationPlayer")
+	anim_player.play("Idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
