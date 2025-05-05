95d961da5d1522e442b0b885cd7d9be4882f7c57
JohnverLavina <johnver.lavina@student.ateneo.edu>
2025-05-05 20:59:30 +0800

    lighting, animations, 3D character scene

--- character_3d.gd
+++ character_3d.gd
@@ -11,12 +11,16 @@ func _ready() -> void:
 	parent = get_tree().get_current_scene()
 	print(parent.get_name())
 	if Global.current_scene.get_name() == "Home View":
-		animation_player.play("Breathing Idle")
-		if animation_player.is_playing():
-			print("Penis")
-
-
+		animation_player.play("Warming Up")
+	#elif Global.current_scene.get_name() == "Workout View":
+		#workout_animation()
+	#else:
+		#pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func workout_animation():
+	#routine.exercises[currentExercise].exercise_name
 	pass
